98ca00c8244b820b0a2483a55c39c10594d6ec74
Balan Andrei <abalan91@yahoo.com>
2024-11-21 13:13:34 +0000

    Add Models for Quizzes. Update Database by creating new tablers based on the model and run the migration.

---
+++ BlazorAppForDataStructures/Models/Answer.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorAppForDataStructures.Models
+{
+    public class Answer
+    {
+        public int AnswerId { get; set; }
+        public int QuestionId { get; set; }
+        public string? AnswerText { get; set; }
+        public bool IsCorrect { get; set; }
+
+        public Question? Question { get; set; }
+    }
+}
---
+++ BlazorAppForDataStructures/Models/Question.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorAppForDataStructures.Models
+{
+    public class Question
+    {
+        public int QuestionId { get; set; }
+        public int TopicId { get; set; }
+        public string? QuestionText { get; set; }
+        public ICollection<Answer>? Answers { get; set; }
+
+        public Topic? Topic { get; set; }
+    }
+}
