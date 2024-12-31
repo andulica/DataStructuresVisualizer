e4f90957101f321e591f7a281b92b81521ef931f
Balan Andrei <abalan91@yahoo.com>
2024-12-31 08:53:12 +0200

    Commented the ProgressBar as it will be a future feature for allowing users to rewind the last operation that they visualized.

---
+++ DataStructuresVisualizer/DataStructures/VisualizationStep.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresVisualizer.DataStructures
+{
+    public class VisualizationStep
+    {
+        public int Index {  get; set; }
+        public string Name { get; set; }
+        public Func<Task> ForwardAction { get; set; }
+        public Func<Task> BackwardAction { get; set; }
+    }
+
+}
