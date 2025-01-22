716b4d01c7409ed5ac6ec184c51adff20385e8a2
Balan Andrei <abalan91@yahoo.com>
2025-01-22 10:11:21 +0200

    1. Refactor the quiz section of the codebase with eliminating duplicates and creating a single razor component to handle all the quizzes. 2. Add spinner for awaiting feedback at the end of the quiz. 3. Reset the radio button after every question in the quiz.

--- BlazorAppForDataStructures/Services/QuizService.cs
+++ BlazorAppForDataStructures/Services/QuizService.cs
@@ -10,10 +10,8 @@ public class QuizService
     }
     public async Task<List<Question>> GetQuestionsAsync(string topicName)
     {
-        // Temporary solution to remove "Quiz" from the topic name. Need to update the API to add quiz substring for all topics.
-        string topicNameWithoutSubstringQuiz = topicName.Replace("Quiz", "");
 
-        var url = $"https://datastructviz-quiz-api-001-hbcza9gdbpb7gzew.canadacentral-01.azurewebsites.net/api/Topics/{topicNameWithoutSubstringQuiz}/questions";
+        var url = $"https://datastructviz-quiz-api-001-hbcza9gdbpb7gzew.canadacentral-01.azurewebsites.net/api/Topics/{topicName}/questions";
 
         var questions = await _httpClient.GetFromJsonAsync<List<Question>>(url);
 
